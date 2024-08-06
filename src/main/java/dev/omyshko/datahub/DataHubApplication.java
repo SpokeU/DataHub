fa6648aae12150b03c1c6a0ac45bb5dd02112c7d
SpokeU <omyshko91@gmail.com>
2024-08-06 20:16:28 +0300

    Update DataHubApplication.java

--- src/main/java/dev/omyshko/datahub/DataHubApplication.java
+++ src/main/java/dev/omyshko/datahub/DataHubApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DataHubApplication {
 
     public static void main(String[] args) {
+        System.out.println("App started");
         SpringApplication.run(DataHubApplication.class, args);
     }
 
